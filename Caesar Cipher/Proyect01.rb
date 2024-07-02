2980d32ff67a0abe11d4dfaa3ecaaea63f0ad7d9
Ale_Pucheta <aavp0316@gmail.com>
2024-07-02 15:40:47 -0600

    Cifrado

---
+++ Caesar Cipher/Proyect01.rb
@@ -0,0 +1,31 @@
+#Segunda Forma
+def cifrar_cesar(texto, desplazamiento)
+    alfabeto = ('a'..'z').to_a
+    texto_cifrado = texto.chars.map do |caracter|
+      if alfabeto.include?(caracter.downcase)
+        indice_actual = alfabeto.find_index(caracter.downcase)
+        indice_nuevo = (indice_actual + desplazamiento) % alfabeto.size
+        caracter_cifrado = alfabeto[indice_nuevo]
+        caracter == caracter.upcase ? caracter_cifrado.upcase : caracter_cifrado
+      else
+        caracter
+      end
+    end
+    texto_cifrado.join
+  end
+  
+  def descifrar_cesar(texto, desplazamiento)
+    cifrar_cesar(texto, -desplazamiento)
+  end
+  
+  puts "Introduce el texto a cifrar:"
+  texto = gets.chomp
+  puts "Introduce el desplazamiento:"
+  desplazamiento = gets.chomp.to_i
+  
+  texto_cifrado = cifrar_cesar(texto, desplazamiento)
+  puts "Texto cifrado: #{texto_cifrado}"
+  
+  texto_descifrado = descifrar_cesar(texto_cifrado, desplazamiento)
+  puts "Texto descifrado: #{texto_descifrado}"
+  
